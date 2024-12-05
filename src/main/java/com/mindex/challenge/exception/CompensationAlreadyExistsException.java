45c17661d1f9ee9a0272430a86cce93ff3a10293
DerekJL <djloisel19@gmail.com>
2024-12-05 11:26:37 -0600

    removed comment

--- src/main/java/com/mindex/challenge/exception/CompensationAlreadyExistsException.java
+++ src/main/java/com/mindex/challenge/exception/CompensationAlreadyExistsException.java
@@ -2,9 +2,6 @@ package com.mindex.challenge.exception;
 
 public class CompensationAlreadyExistsException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 
 	public CompensationAlreadyExistsException(String message) {
