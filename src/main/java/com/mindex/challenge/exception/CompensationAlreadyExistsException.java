87c719cb0a6e48ee6a4a2b17b100130eb5456860
DerekJL <djloisel19@gmail.com>
2024-12-05 11:24:45 -0600

    added gitignore, json error responses, custom exception

---
+++ src/main/java/com/mindex/challenge/exception/CompensationAlreadyExistsException.java
@@ -0,0 +1,13 @@
+package com.mindex.challenge.exception;
+
+public class CompensationAlreadyExistsException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public CompensationAlreadyExistsException(String message) {
+		super(message);
+	}
+}
